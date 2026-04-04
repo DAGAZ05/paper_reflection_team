789f897b3e2e552b14555c44f960b6f62d285952
Zixun Wang <3087485718@qq.com>
2026-04-04 23:31:36 +0800

    最终项目源码及文档

--- member_a/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.fixture(scope="session", autouse=True)
-def _configure_asyncio():
-    """pytest-asyncio 默认使用 auto mode"""
-    pass
--- member_b/member_b/__main__.py
+++
@@ -1,3 +0,0 @@
-from .member_b_module import run_dedup, run_evidence_validation
-
-__all__ = ["run_dedup", "run_evidence_validation"]
